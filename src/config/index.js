3f11cde13224271f752297a281b41ab7c7912747
lihaitao <15393243613@163.com>
2018-12-07 14:04:01 +0800

    add config

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
  * @Author: lihaitao
  * @Date: 2018-12-06 11:04:29
  * @Last Modified by: lihaitao
- * @Last Modified time: 2018-12-06 11:04:50
+ * @Last Modified time: 2018-12-06 11:12:55
  */
 export default {
 }
