e0494e5bfeacd4b821a349dfdb2f61a15eaccb24
lihaitao <15393243613@163.com>
2018-12-10 16:34:52 +0800

    add normalize.css #19

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router/router.js'
 import store from '@/store/index.js'
+import 'normalize.css'
 import '@/plugins/element.js'
 Vue.config.productionTip = false
 new Vue({
