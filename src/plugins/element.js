64ff52d1b827b2d3f48a632d9a416de396fe5f9a
李海涛 <liht@7moor.com>
2018-12-13 21:44:33 +0800

    add 无限嵌套菜单

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,11 @@ import {
   Header,
   Aside,
   Main,
-  Footer
+  Footer,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup
 } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Button)
@@ -14,3 +18,7 @@ Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
 Vue.use(Footer)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
