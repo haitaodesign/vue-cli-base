308e317d44d66f63b80c79798eafb88ac85d42cb
haitaodesign <15393243613@163.com>
2018-12-18 00:00:44 +0800

    add vuex拆分模块文件自动加载

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,4 +1,10 @@
-import ModulesOne from './moduleOne'
-export default {
-  ModulesOne
-}
+const contexts = require.context('.', true, /\.js$/)
+let modules = {}
+contexts.keys().forEach(key => {
+  const index = key.split('/')[2]
+  const contextName = key.split('/')[1]
+  if (index === 'index.js') {
+    modules[`${contextName}`] = contexts(key).default
+  }
+})
+export default modules
