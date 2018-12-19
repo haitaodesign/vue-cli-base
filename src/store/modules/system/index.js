3e246d32673f48b8fc582d0f34de7df91b90e055
haitaodesign <15393243613@163.com>
2018-12-19 16:44:00 +0800

    add store调用mockjs数据

---
+++ src/store/modules/system/index.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: lihaitao
+ * @Date: 2018-12-19 11:44:55
+ * @Last Modified by: lihaitao
+ * @Last Modified time: 2018-12-19 14:47:51
+ */
+import { SET_COLLAPSE, SET_MENULIST } from './mutation-types'
+import SystemApi from '../../../api/system.js'
+const systemApi = new SystemApi()
+export default {
+  namespaced: true,
+  state: {
+    isCollapse: false,
+    menuList: []
+  },
+  getters: {
+  },
+  mutations: {
+    [SET_COLLAPSE] (state, payload) {
+      state.isCollapse = payload
+    },
+    [SET_MENULIST] (state, payload) {
+      state.menuList = payload
+    }
+  },
+  actions: {
+    async getMenuList ({ commit }, payload) {
+      try {
+        const res = await systemApi.getMenuList()
+        const data = res.data
+        commit('SET_MENULIST', data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
---
+++ src/store/modules/system/mutation-types.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: lihaitao
+ * @Date: 2018-12-19 11:45:04
+ * @Last Modified by:   lihaitao
+ * @Last Modified time: 2018-12-19 11:45:04
+ */
+export const SET_COLLAPSE = 'SET_COLLAPSE'
+export const SET_MENULIST = 'SET_MENULIST'
