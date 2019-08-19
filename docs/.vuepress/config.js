8608f4cc3ffb297b9b06118710d862cb6db3187d
haitaodesign <15393243613@163.com>
2019-08-19 11:18:33 +0800

    docs: 修改 docs 配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: '数据运营系统技术文档',
+  title: '基于 Vue-Cli 3 的企业级项目模板架构',
   description: '技术架构，组件演示，开发日志...',
   head: [
     ['link', { rel: 'shortcut icon', href: '/favicon.ico'}]
@@ -11,7 +11,7 @@ module.exports = {
       { text: '开发规范', link: '/specs/' },
       { text: '组件文档', link: '/components/' },
       { text: '开发日志', link: '/changelog/'},
-      { text: 'GitLab', link: 'http://git.7moor.com/do/expo/tree/master'}
+      { text: 'GitHub', link: 'https://github.com/haitaodesign/Vue-Cli3.0-Enterprise-Boilerplate'}
     ],
     sidebar: {
       '/components/': [
