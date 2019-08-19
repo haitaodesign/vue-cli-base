3ad260a84de9daba469755ca9b1c3d6d3140ffa9
haitaodesign <15393243613@163.com>
2019-08-19 10:15:49 +0800

    refactor: 删除无用的模块引用

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store/index.js'
 import 'normalize.css'
-import _ from '@/plugins/lodash/index.js'
 import util from '@/utils/index.js'
-Vue.prototype._ = _
 Vue.prototype.util = util
 Vue.config.productionTip = false
 new Vue({
