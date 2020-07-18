cd953a99391e57f36d316d15f23165d0f6fb6e99
haitaodesign <lihaitaodesign@gmail.com>
2020-07-18 15:06:40 +0800

    refactor: 修改components 为 duerojs

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from '@/router'
 import store from '@/store/index.js'
 import 'normalize.css'
 import util from '@/utils/index.js'
-import '@/plugins/components/index.js'
+import '@/plugins/duerojs/index.js'
 Vue.prototype.util = util
 Vue.config.productionTip = false
 new Vue({
